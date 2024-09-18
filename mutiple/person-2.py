11cd22d3563b678b8e9fd59e4f5748b2cf7ac284
dattiendo <dattiendo2005@gmail.com>
2024-09-18 09:22:28 +0700

    Add files via upload

---
+++ mutiple/person-2.py
@@ -0,0 +1,5 @@
+class person:
+    def sleep(self):
+        return "sleeping..."
+
+    
---
+++ mutiple/teacher.py
@@ -0,0 +1,4 @@
+from employee import Employee
+class Teacher(person, Employee):
+    def teach(self):
+        return "teaching..."
